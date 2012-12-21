72dc508078ed322f6887f870f6e82514b45b457e
GrayB <gray.bowman@gmail.com>
2012-12-21 11:12:49 -0500

    Added agent app and Sighting model

---
+++ agent/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Sighting(models.Model):
+    host = models.CharField(max_length=32)
+    device_id = models.CharField(max_length=32)
+    signal_dbm = models.IntegerField()
+    timestamp = models.IntegerField()
