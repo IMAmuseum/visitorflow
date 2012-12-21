7453ef21c92745dc3a6055a4a0b20f8f599095e2
Gray Bowman <gray.bowman@gmail.com>
2012-12-21 10:42:44 -0500

    updating db host

--- visitorflow/settings.py
+++ visitorflow/settings.py
@@ -15,7 +15,7 @@ DATABASES = {
         'NAME': 'visitorflow',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': 'root',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
