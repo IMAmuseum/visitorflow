6be1a3f7cb4bf194770f830cb6bb5b7f3576f09a
GrayB <gray.bowman@gmail.com>
2012-12-21 12:13:20 -0500

    added agent report view, wsgi setup, agent app inclusion in settings

--- agent/views.py
+++ agent/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+from django.http import HttpResponse, HttpResponseBadRequest
+from agent.models import Sighting
+
+
+def Report(request):
+    if (request.method == 'POST'
+      and request.POST.get('host') is not None
+      and request.POST.get('device_id') is not None
+      and request.POST.get('timestamp') is not None
+      and request.POST.get('signal_dbm') is not None):
+
+        # create a new sighting from the request
+        Sighting.objects.create(
+            host=request.POST.get('host'),
+            device_id=request.POST.get('device_id'),
+            timestamp=request.POST.get('timestamp'),
+            signal_dbm=request.POST.get('signal_dbm'))
+        return HttpResponse()
+    else:
+        return HttpResponseBadRequest()
