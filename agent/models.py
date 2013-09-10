2784b76650ec1ed07dd048a4ff26f7c5951ae78f
Gray Bowman <gray.bowman@gmail.com>
2013-09-10 16:18:43 -0400

    added agent normalized sighting model and command

--- agent/models.py
+++ agent/models.py
@@ -5,5 +5,15 @@ class Sighting(models.Model):
     host = models.CharField(max_length=32)
     device_id = models.CharField(max_length=32)
     signal_dbm = models.IntegerField()
-    timestamp = models.IntegerField()
-    processed = models.BooleanField(default=False)
+    timestamp = models.IntegerField(db_index=True)
+    normalize_processed = models.BooleanField(default=False)
+
+
+class NormalizedSighting(models.Model):
+	host = models.CharField(max_length=32)
+	device_id = models.CharField(max_length=32)
+	signal_low = models.IntegerField()
+	signal_high = models.IntegerField()
+	signal_avg = models.IntegerField()
+	num_samples = models.IntegerField()
+	timestamp = models.IntegerField()
